e5b260d38f27c366f6bc8da012d873528d533f1b
mnsternik <mnsternik@gmail.com>
2024-06-14 14:28:13 +0200

    Getting failrure decision from user added

--- utilities/Step/Step.cs
+++ utilities/Step/Step.cs
@@ -10,7 +10,7 @@ namespace TestRunnerConsole
         public string? ElementId { get; set; } = elementId;
         public string? BackupScenarioPath { get; set; } = backupScenarioPath;
 
-        public void ExecuteAndLog()
+        public void ExecuteAndLog(int stepCounter)
         {
             try
             {
@@ -21,25 +21,26 @@ namespace TestRunnerConsole
             catch (InavlidVerificationException e)
             {
                 Logger.Log($"Wystąpił błąd podczas weryfikacji. {e.Message}", true);
-                HandleFailure();
+                HandleFailure(stepCounter, e.Message);
             }
             catch (NoSuchElementException e)
             {
                 Logger.Log($"Wystąpił błąd - nie znaleziono elementu na stronie. {e.Message}", true);
-                HandleFailure();
+                HandleFailure(stepCounter, e.Message);
             }
             catch (Exception e)
             {
                 Logger.Log($"Wystąpił nieoczekiwany błąd: {e.Message}", true);
-                HandleFailure();
+                HandleFailure(stepCounter, e.Message);
             }
         }
 
+        public virtual void HandleAction()
+        {
+            // Każdy klasa pochodna posiada własną implementacje metody HandleAction
+        }
 
-        // Każdy klasa pochodna posiada własną implementacje metody HandleAction
-        public virtual void HandleAction() {}
-
-        public void HandleFailure()
+        public void HandleFailure(int stepCounter, string message)
         {
             if (!string.IsNullOrEmpty(BackupScenarioPath))
             {
@@ -47,14 +48,18 @@ namespace TestRunnerConsole
             }
             else
             {
-                string decision = GetFailureDecisionFromUser();
-                if (decision == "N")
+                string decision = UserInputUtility.GetFailureDecisionFromUser();
+                switch (decision)
                 {
-                    throw new Exception("Wykonywanie scenariusza przerwane. Koniec działania programu.");
-                }
-                else if (decision == "R")
-                {
-                    ExecuteAndLog(); 
+                    case "Y": // dodaj błąd do podsumowania i przejdź do kolejnego kroku
+                        Summary.AddFailedStep(stepCounter, message);
+                        break;
+                    case "R": // wykonaj krok ponownie
+                        ExecuteAndLog(stepCounter);
+                        break;
+                    case "N": // dodaj błąd do posumowania i zakończ działanie programu 
+                        Summary.AddFailedStep(stepCounter, message); 
+                        throw new FailedStepException("Wykonywanie scenariusza zostało przerwane.");
                 }
             }
         }
@@ -79,27 +84,15 @@ namespace TestRunnerConsole
                 throw new InavlidStepParameterException($"Wykryto próbe odnalezienia elementu bez podania jego ID lub XPath - potrzebna weryfikacja poprawności scenariusza testowego.");
             }
         }
-
-        private string GetFailureDecisionFromUser()
-        {
-            Logger.Log("Wstrzymano wykonywanie scenariusza testowego. Wpisz 'Y' by przejść do kolejnego kroku, 'R' by spróbować wykonać krok ponownie, lub 'N' by zakończyć:';");
-            string? input = Console.ReadLine()?.Trim().ToUpper();
-
-            while (true)
-            {
-                if (input == "Y" || input == "N" || input == "R")
-                {
-                    Logger.Log($"Udzielono odpowiedzi: {input}", true);
-                    return input;
-                }
-                Logger.Log("Wprowadzono niepoprawną odpowiedź - wprowadź 'Y' by kontynuować lub 'N' by przerwać: ");
-                input = Console.ReadLine()?.Trim().ToUpper();
-            }
-        }
     }
 
     public class InavlidStepParameterException : Exception
     {
         public InavlidStepParameterException(string message) : base(message) { }
+    }
+
+    public class FailedStepException : Exception
+    {
+        public FailedStepException(string message) : base(message) { }
     }
 }
