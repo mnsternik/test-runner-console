ccec333553b04b3320fa8e143f9e12161c27a367
mnsternik <mnsternik@gmail.com>
2024-07-03 04:34:28 +0200

    TransformIntoSpecifedStep method moved to GenericStep class

--- Step/GenericStep.cs
+++ Step/GenericStep.cs
@@ -7,13 +7,28 @@ namespace TestRunnerConsole
         public string? Url { get; set; } = string.Empty;
         public string? ElementId { get; set; } = string.Empty;
         public string? ElementXPath { get; set; } = string.Empty;
-        public string? ContextId { get; set; } = string.Empty; 
+        public string? ContextId { get; set; } = string.Empty;
         public string? Value { get; set; } = string.Empty;
         public string? ExpectedValue { get; set; } = string.Empty;
-        public string? OptionType { get; set; } = string.Empty; 
+        public string? OptionType { get; set; } = string.Empty;
         public string? CheckType { get; set; } = string.Empty;
         public bool? CheckInsideSelect { get; set; } = false;
-        public string? BackupScenarioPath { get; set; } = string.Empty; 
+        public string? BackupScenarioPath { get; set; } = string.Empty;
+
+        public static Step TransformIntoSpecifedStep(GenericStep step)
+        {
+            return step.Action switch
+            {
+                "navigate" => new NavigateStep(step),
+                "click" => new ClickStep(step),
+                "select" => new SelectStep(step),
+                "verify" => new VerifyStep(step),
+                "iframe-change" => new ChangeContextStep(step),
+                "manual" => new ManualStep(step),
+                "write" or "write-login" or "write-password" => new WriteStep(step),
+                _ => throw new InavlidStepParameterException($"Nieprawidłowy rodzaj akcji ActionType: '{step.Action}'"),
+            };
+        }
     }
 }
 
